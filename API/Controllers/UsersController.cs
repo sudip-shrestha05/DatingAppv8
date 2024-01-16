47dc010b1512096c082743b8f54e45f6d0226f65
Sudip Shrestha <sudipshresthaus@hotmail.com>
2024-01-16 15:22:13 +0000

    Section1 Skeletion API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route(V)] //api.users
+public class UsersController : ControllerBase
+{
+    private const string V = "api/[controller]";
+    private const string V1 = "{id}";
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] //api/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet(V1)] //api/users2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
